0241d9e4de98c10a070adbbe140a3014c9228179
sarah <sarah.endres@pentlandfirth.com>
2025-10-02 09:54:03 +0200

    create config json and adjust config for building exe

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
 import socket
-
+import os
+import json
 
 CACHE = True
 IMAGE_SIZE=2000
@@ -13,33 +14,45 @@ DATASET_NAME="gemuese_netz_sub"
 
 HOSTNAME = socket.gethostname()
 
-if HOSTNAME == "niggis-brain":
-    DATASET_DIR = f"/media/fast/dataset/bildunterschied/labeling/{DATASET_NAME}"
-    UI_SCALING=2.5
-    FONT_SCALING=2.5
-    DATASET_NAME="tmp"
-    DATASET_DIR = f"/media/fast/dataset/bildunterschied/test_mini/new_label_tool/{DATASET_NAME}"
-    USERNAME = "niklas"
-elif HOSTNAME == "niklas-XPS-15-9530":
-    UI_SCALING=2.5
-    FONT_SCALING=2.5
-    DATASET_NAME="dev"
-    DATASET_DIR = f"/home/niklas/dataset/bildunterschied/{DATASET_NAME}"
-    USERNAME = "niklas"
-elif HOSTNAME == "sarah-XPS-15-9530":
-    USERNAME = "sarah"
-    DATASET_NAME = "sarah_20250801-20250816"
-    # DATASET_DIR = f"/home/sarah/Documents/background_segmentation/{DATASET_NAME}"
-    LOCAL_LOG_DIR = f"/home/sarah/Documents/change_detection/label-image-chang/local_log_dir"
-    SEGMENTATION_PATH = f"/home/sarah/Documents/change_detection/local_paths/segmented_boxes"
-    DATASET_DIR = f"/home/sarah/Documents/data/{DATASET_NAME}"
 
-elif HOSTNAME == "NB-ENDRES":
-    USERNAME = "sarah_windoof"
-    DATASET_NAME = "example_sessions"
-    # DATASET_DIR = f"/home/sarah/Documents/background_segmentation/{DATASET_NAME}"
-    DATASET_DIR = rf"C:\Users\sarah.endres\Documents\{DATASET_NAME}"
-
+# 1. Versuch: user_config.json laden
+if os.path.exists("user_config.json"):
+    with open("user_config.json", "r", encoding="utf-8") as f:
+        cfg = json.load(f)
+    USERNAME = cfg.get("username", "unknown")
+    DATASET_NAME = cfg.get("dataset_name", "default")
+    DATASET_DIR = cfg.get("dataset_dir", "./dataset")
+    SERVER = cfg.get("server", SERVER)
+    print(f"[CONFIG] loaded from user_config.json: {USERNAME}, {DATASET_DIR}")
 else:
-    raise Exception("Unknown host", HOSTNAME)
+
+    if HOSTNAME == "niggis-brain":
+        DATASET_DIR = f"/media/fast/dataset/bildunterschied/labeling/{DATASET_NAME}"
+        UI_SCALING=2.5
+        FONT_SCALING=2.5
+        DATASET_NAME="tmp"
+        DATASET_DIR = f"/media/fast/dataset/bildunterschied/test_mini/new_label_tool/{DATASET_NAME}"
+        USERNAME = "niklas"
+    elif HOSTNAME == "niklas-XPS-15-9530":
+        UI_SCALING=2.5
+        FONT_SCALING=2.5
+        DATASET_NAME="dev"
+        DATASET_DIR = f"/home/niklas/dataset/bildunterschied/{DATASET_NAME}"
+        USERNAME = "niklas"
+    elif HOSTNAME == "sarah-XPS-15-9530":
+        USERNAME = "sarah"
+        DATASET_NAME = "sarah_20250801-20250816"
+        # DATASET_DIR = f"/home/sarah/Documents/background_segmentation/{DATASET_NAME}"
+        LOCAL_LOG_DIR = f"/home/sarah/Documents/change_detection/label-image-chang/local_log_dir"
+        SEGMENTATION_PATH = f"/home/sarah/Documents/change_detection/local_paths/segmented_boxes"
+        DATASET_DIR = f"/home/sarah/Documents/data/{DATASET_NAME}"
+
+    elif HOSTNAME == "NB-ENDRES":
+        USERNAME = "sarah_windoof"
+        DATASET_NAME = "example_sessions"
+        # DATASET_DIR = f"/home/sarah/Documents/background_segmentation/{DATASET_NAME}"
+        DATASET_DIR = rf"C:\Users\sarah.endres\Documents\{DATASET_NAME}"
+
+    else:
+        raise Exception("Unknown host", HOSTNAME)
 
