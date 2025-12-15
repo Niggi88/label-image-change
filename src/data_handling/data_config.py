6969fc8829ad32ff72f47dcfb00257fb94c3a6ab
niklas <niklas.unverricht@pentlandfirth.com>
2025-12-15 12:20:48 +0100

    add ml2 and local hosts

--- src/data_handling/data_config.py
+++ src/data_handling/data_config.py
@@ -4,15 +4,19 @@ from pathlib import Path
 hostname = socket.gethostname()
 
 
-
-print(hostname)
-
 if hostname == "niklas-XPS-15-9530":
     _base_data_dir = Path("/home/niklas/dataset/snapshot_change_detection/datasets")
-    raw_data = _base_data_dir / "raw"
-    override_root = raw_data / "images"
-    _out_dataset_name = "mock_tiny_test"
-    out_datasets_dir = _base_data_dir / "datasets" / _out_dataset_name
-    _src_data_name = "santiago"
-    IMAGE_SIZE = 416
-    # "/media/fast/dataset/bildunterschied/real_data/test_sarah/images"
+elif hostname == "ml02":
+    _base_data_dir = Path("/home/niklas.unverricht/dataset/snapshot_change_detection")
+elif hostname == "niggis-brain":
+    _base_data_dir = Path("/media/fast/dataset/snapshot_change_detection")
+
+
+# _out_dataset_name = "large_xl-images_v1"
+_out_dataset_name = "testset_xl-images_v1"
+# src_data_names = ["santiago", "sarah", "almas"]
+src_data_names = ["niklas"]
+IMAGE_SIZE = 832
+raw_data = _base_data_dir / "change_data"
+override_root = raw_data / "images"
+out_datasets_dir = _base_data_dir / "datasets" / _out_dataset_name
