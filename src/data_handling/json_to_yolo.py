e05abc0478b9bded96319c7beb4e164effbefc74
niklas <niklas.unverricht@pentlandfirth.com>
2026-02-11 11:28:43 +0100

    prepare santiago data

--- src/data_handling/json_to_yolo.py
+++ src/data_handling/json_to_yolo.py
@@ -136,10 +136,10 @@ def export_session(annotation_file, index, yolo_splitted_paths: YoloPathsSplit, 
 
 if __name__ == "__main__":
     
-    all_annotators = ["santiago", "niklas", "sarah", "almas"]
+    all_annotators = ["niklas", "sarah", "almas", "santiago"]
 
-    train_set_base_name = "images_v3_0_rem"
-    test_set_base_name = "testset_images_v3_0_rem"
+    train_set_base_name = "images_v3_0"
+    test_set_base_name = "testset_images_v3_0"
 
     # _out_dataset_name = "testset_xl-images_v3_0"
     # src_data_names = ["santiago", "sarah", "almas"]
