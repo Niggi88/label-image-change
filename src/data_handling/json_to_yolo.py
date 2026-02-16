2bdb28d3219ed5f6f3ab672636f1a1eb23081478
niklas <niklas.unverricht@pentlandfirth.com>
2026-02-16 12:21:21 +0100

    prepare reproduction of sarah data

--- src/data_handling/json_to_yolo.py
+++ src/data_handling/json_to_yolo.py
@@ -142,7 +142,7 @@ def export_session(annotation_file, index, yolo_splitted_paths: YoloPathsSplit, 
 
 if __name__ == "__main__":
     
-    all_annotators = ["niklas", "sarah", "almas", "santiago"]
+    all_annotators = ["niklas", "almas", "santiago", "sarah"]
 
     train_set_base_name = "images_v3_0"
     test_set_base_name = "testset_images_v3_0"
